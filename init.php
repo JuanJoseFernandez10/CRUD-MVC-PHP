a37abf19affd91f511594693a8a4bf62ebe3162f
Juan Jose Fernandez <fernandez.suarez.juan.jose@iescamas.es>
2025-12-11 11:31:28 +0100

    Modificacion de el archivo init introduciondo los datos de mi hostign

--- init.php
+++ init.php
@@ -6,8 +6,8 @@ date_default_timezone_set('America/Sao_Paulo');
 
 // conexão com o banco de dados
 
-define('BD_SERVIDOR','localhost');
-define('BD_USUARIO','root');
-define('BD_SENHA','');
-define('BD_BANCO','livraria');
+define('BD_SERVIDOR','mysql-fernandezjuanjose.alwaysdata.net');
+define('BD_USUARIO','445581');
+define('BD_SENHA','JJFS2006.');
+define('BD_BANCO','fernandezjuanjose_mysql');
 
